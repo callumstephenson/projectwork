ea63c8ed8d26c32f3055dbe21c666659bcaa9b98
callumstephenson <callumstephenson03@gmail.com>
2021-11-08 18:25:28 +0000

    factor list changed to include all possible prime factors

--- euler_problems/euler3.py
+++ euler_problems/euler3.py
@@ -1,12 +1,13 @@
-def lpf(number):
+def hpf(number):
     factor_list = []
     prime_list = []
     isPrime = False
-    for i in range(2, int(number**0.5+1)):
+    for i in range(2, int(number**0.5)+1):
         if number % i == 0:
             factor_list.append(i)
+            factor_list.append(number/i)
     for j in range(len(factor_list)):
-        for n in range (2, int(factor_list[j]**0.5+1)):
+        for n in range (2, int(factor_list[j]**(0.5) +1)):
             if factor_list[j] % n == 0:
                 isPrime = False
                 break
@@ -15,5 +16,5 @@ def lpf(number):
         if isPrime == True:
             prime_list.append(factor_list[j])
     return max(prime_list)
-    
-print(lpf(600851475143))
+
+print(hpf(600851475143))
