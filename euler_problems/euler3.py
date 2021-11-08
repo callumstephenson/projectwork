82b047a51deb2ead6bcde6cec9fe68f0bf0870ec
callumstephenson <callumstephenson03@gmail.com>
2021-11-08 19:00:52 +0000

    changed loop var names

--- euler_problems/euler3.py
+++ euler_problems/euler3.py
@@ -6,20 +6,21 @@ def hpf(number):
     # Can do this due to sqrt(x)*sqrt(x) = x, therefore for each factor, 
     # one factor is always less than or equal to sqrt(x), hence by only
     # checking up to sqrt(x) and generating converse factor, saves time.
-    for potentialFactor in range(2, int(number**0.5)+1):
-        if number % potentialFactor == 0:
-            factor_list.append(potentialFactor)
+    for i in range(2, int(number**0.5)+1):
+        if number % i == 0:
+            factor_list.append(i)
     # Generating converse factor to append list with num/factor
-            factor_list.append(number/potentialFactor)
+            factor_list.append(number/i)
     # Checking for primes in the factor list
-    for factor in range(len(factor_list)):
-        for n in range (2, int(factor_list[factor]**(0.5) +1)):
-            if factor_list[factor] % n == 0:
+    for j in range(len(factor_list)):
+        for n in range (2, int(factor_list[j]**(0.5) +1)):
+            if factor_list[j] % n == 0:
+                isPrime = False
                 break
             else:
                 isPrime = True
         if isPrime == True:
-            prime_list.append(factor_list[factor])
+            prime_list.append(factor_list[j])
     return max(prime_list)
 
 print(hpf(600851475143))
