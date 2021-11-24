527810e83493707d57b9c08ed47abb5b2161cccb
callumstephenson <36637180+callumstephenson@users.noreply.github.com>
2021-11-24 01:41:53 +0000

    Update audiovisualiser.py
    added iteration feature

--- audiovisualiser.py
+++ audiovisualiser.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 from google.colab import drive
-def spectragram(inputPath, outputPath, cmapInput='binary', figsizeInput=(6.4,4.8), hop_lengthInput=512 , n_fftInput=2048, offsetInput=0):
+def imageProcessing(inputPath, outputPath, cmapInput='binary', figsizeInput=(6.4,4.8), hop_lengthInput=512 , n_fftInput=2048, offsetInput=0, colourIteration=False):
   '''A function to iterate over a directory filled with .mp3 files and decompose them using an FFT into .pngs. 
   inputPath = input folder path
   outputPath = output folder path
@@ -15,12 +15,13 @@ def spectragram(inputPath, outputPath, cmapInput='binary', figsizeInput=(6.4,4.8
   n_fftInput = length of the windowed signal after padding with zeros, recommended to set to a power of 2.
   cmapInput = colour map for the resulting image
   offsetInput = offset from start of song to generate audio
+  colourIteration = if you want the program to iterate over all colour maps
   pre-requisite packages: numpy (as np), matplotlib.pyplot(as plt), librosa, librosa.display, os'''
   
   musicFolder = os.listdir(str(inputPath))
   i=0
   FileAmount = len(musicFolder)
-  for musicFile in musicFolder:
+  for musicFile in musicFolder: 
     # File handling and output namescheme
     InputPath = str(inputPath) + musicFile
     OutputPath = str(outputPath) + musicFile[0:-4] + '_image.png'
@@ -32,6 +33,12 @@ def spectragram(inputPath, outputPath, cmapInput='binary', figsizeInput=(6.4,4.8
     DB = librosa.amplitude_to_db(D, ref=np.max)
     librosa.display.specshow(DB, sr=sr, hop_length=hop_lengthInput, x_axis='time', y_axis='log');
     #Set colourmap of output image.
-    plt.set_cmap(str(cmapInput))
-    plt.axis('off')
-    plt.savefig(OutputPath,bbox_inches='tight')
+    if colourIteration == False:
+      plt.set_cmap(str(cmapInput))
+      plt.axis('off')
+      plt.savefig(OutputPath,bbox_inches='tight')
+    else:
+      for colourmap in matplotlib.plr.colormaps():
+        plt.set_cmap(str(colourmap))
+        plt.axis('off')
+        plt.savefig(OutputPath,bbox_inches='tight')
