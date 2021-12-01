ee0124532ac4c73255124d120d3caf6024bf7cff
callumstephenson <callumstephenson03@gmail.com>
2021-12-01 03:05:19 +0000

    changed Z

--- 3dplots.py
+++ 3dplots.py
@@ -2,20 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# set up plot and assign x,y,z values
 fig = plt.figure()
 ax = Axes3D(fig)
 X = np.arange(-4, 4, 0.25)
 Y = np.arange(-4, 4, 0.25)
 X, Y = np.meshgrid(X, Y)
 R = np.sqrt(X ** 2 + Y ** 2)
-Z = np.cos(R)
+Z = np.cos(R) + np.sin(3*X)
 
+# solid contour plot
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.hot)
 ax.contourf(X, Y, Z, zdir='z', offset=-2, cmap=plt.cm.hot)
 ax.set_zlim(-2, 2)
 plt.axis("off")
 plt.show()
 
+# wireframe contour plot
 ax=plt.axes(projection='3d')
 ax.contourf(X, Y, Z, 25, cmap='hot')
 plt.axis("off")
